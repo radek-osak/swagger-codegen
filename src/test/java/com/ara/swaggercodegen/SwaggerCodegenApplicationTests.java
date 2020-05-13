77a8bc52d8f4772128a0df1bc030a4a2fa954a08
Radek Osak <radek030790@gmail.com>
2020-05-13 10:30:51 +0200

    initial commit

---
+++ src/test/java/com/ara/swaggercodegen/SwaggerCodegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ara.swaggercodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCodegenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
